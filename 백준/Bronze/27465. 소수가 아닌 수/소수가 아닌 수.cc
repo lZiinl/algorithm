4c7c199654908bf3c7c6f9235957586b300cde01
lZiinl <149471946+lZiinl@users.noreply.github.com>
2024-07-06 16:58:06 +0900

    [Bronze III] Title: 소수가 아닌 수, Time: 0 ms, Memory: 2020 KB, Score: 5 point -BaekjoonHub

---
+++ 백준/Bronze/27465. 소수가 아닌 수/소수가 아닌 수.cc
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+long long int N;
+
+void input() {
+    cin >> N;
+}
+
+void solve() {
+    cout << 10;
+}
+
+int main() {
+
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    //freopen("sample.txt", "r", stdin);
+
+    input();
+    solve();
+
+    return 0;
+}
