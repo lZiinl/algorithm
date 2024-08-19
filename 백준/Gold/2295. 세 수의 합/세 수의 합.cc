41b9a6f1fef6dcdaaafe67f39522dd36a07fc012
lZiinl <149471946+lZiinl@users.noreply.github.com>
2024-08-19 22:15:11 +0900

    [Gold IV] Title: 세 수의 합, Time: 44 ms, Memory: 5228 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.cc
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.cc
@@ -7,50 +7,63 @@ using namespace std;
 
 int N;
 int num[1001];
-vector<int> sum;
-bool flag;
+vector<int>sum;
+int flag;
 
 void input() {
+
     cin >> N;
+
     for (int i = 0; i < N; i++) {
         cin >> num[i];
     }
-    sort(num, num + N);
 
     for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
+        for (int j = i; j < N; j++) {
             sum.push_back(num[i] + num[j]);
         }
     }
 
+    sort(num, num + N);
     sort(sum.begin(), sum.end());
 }
 
-bool bs(int target) {
+void bs(int s) {
+
     int sp = 0;
     int ep = N - 1;
 
-    while (sp <= ep) {
-        int mid = (sp + ep) / 2;
-        if (num[mid] == target) {
-            return true;
+    while (ep > sp) {
+
+        int temp = num[(sp + ep) / 2];
+
+        if (s == temp) {
+            flag = 1;
+            return;
         }
-        if (num[mid] < target) {
-            sp = mid + 1;
-        } else {
-            ep = mid - 1;
+        else if (s < temp) {
+            ep = (sp + ep) / 2;
+        }
+        else {
+            sp = (sp + ep) / 2 + 1;
         }
     }
-    return false;
 }
 
 void solve() {
+
     for (int i = N - 1; i >= 0; i--) {
+
         int tar = num[i];
+
         for (int j = 0; j < sum.size(); j++) {
+
             if (tar < sum[j]) break;
-            if (bs(tar - sum[j])) {
-                cout << num[i] << endl;
+
+            bs(tar - sum[j]);
+
+            if (flag == 1) {
+                cout << num[i];
                 return;
             }
         }
@@ -58,9 +71,12 @@ void solve() {
 }
 
 int main() {
+
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
+
+    //freopen("sample.txt", "r", stdin);
 
     input();
     solve();
