9dbf137fe301a67a7adf14716aabfa4fd59c2d80
lZiinl <149471946+lZiinl@users.noreply.github.com>
2024-08-23 22:40:14 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_MO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+char c[1001];
+
+void input() {
+	
+}
+
+void solve() {
+	
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	//freopen("sample.txt", "r", stdin);
+
+	//input();
+	int i;
+	cin >> c >> i;
+	cout << c[i-1];
+	//solve();
+
+	return 0;
+}
