3f670b751a99eec203c99db28c0640ccf14ffa7d
lZiinl <149471946+lZiinl@users.noreply.github.com>
2024-08-23 22:29:29 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_MO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int T;
+
+void input() {
+	
+}
+
+void solve() {
+	
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	//freopen("sample.txt", "r", stdin);
+
+	//input();
+
+	cin >> T;
+	for (int i = 0; i < T; i++){
+		int A, B;
+		cin >> A >> B;
+		cout << A + B << "\n";
+	}
+	//solve();
+
+	return 0;
+}
