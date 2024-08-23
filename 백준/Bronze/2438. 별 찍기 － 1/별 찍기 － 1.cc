13df19ab93b355368480740745cbcab8994eb322
lZiinl <149471946+lZiinl@users.noreply.github.com>
2024-08-23 22:25:21 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.cc
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_MO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+void input() {
+	cin >> N;
+}
+
+void solve() {
+	for (int i = 1; i <= N; i++){
+		for (int j = 0; j < i; j++){
+			cout << "*";
+		}
+		cout << "\n";
+	}
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	//freopen("sample.txt", "r", stdin);
+
+	input();
+	solve();
+
+	return 0;
+}
