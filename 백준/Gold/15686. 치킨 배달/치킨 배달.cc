ea81a58439567a8756b7232143d2cbd9215d0cc2
lZiinl <149471946+lZiinl@users.noreply.github.com>
2024-08-26 00:59:27 +0900

    [Gold V] Title: 치킨 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/15686. 치킨 배달/치킨 배달.cc
+++ 백준/Gold/15686. 치킨 배달/치킨 배달.cc
@@ -7,21 +7,21 @@ using namespace std;
 
 int N, M;
 
-struct Node{
+struct Node {
 	int y, x;
 };
 
 vector<Node> house;
 vector<Node> chicken;
-vector<Node> check;
+vector<int> check;
 
 int nmin = 21e8;
 
 void input() {
 	cin >> N >> M;
 
-	for (int i = 1; i <= N; i++){
-		for (int j = 1; j <= N; j++){
+	for (int i = 1; i <= N; i++) {
+		for (int j = 1; j <= N; j++) {
 			int val;
 			cin >> val;
 
@@ -38,15 +38,14 @@ void input() {
 void dfs(int n, int level) {
 
 	if (level == M) {
-		
 		int sum = 0;
-		for (int i = 0; i < house.size(); i++){
+
+		for (int i = 0; i < house.size(); i++) {
 			int dist = 21e8;
 			int r = 0;
 
-			for (int j = 0; j < check.size(); j++){
-				r = abs(house[i].x - check[j].x) + abs(house[i].y - check[j].y);
-				
+			for (int j = 0; j < check.size(); j++) {
+				r = abs(house[i].x - chicken[check[j]].x) + abs(house[i].y - chicken[check[j]].y);
 				if (dist > r) dist = r;
 			}
 			sum += dist;
@@ -56,20 +55,20 @@ void dfs(int n, int level) {
 		return;
 	}
 
-	for (int i = n; i < chicken.size(); i++){
+	for (int i = n; i < chicken.size(); i++) {
 
 		if (chicken.size() - i < M - check.size()) return;
 
-		check.push_back({chicken[i].y , chicken[i].x});
+		check.push_back(i);
 		dfs(i + 1, level + 1);
 		check.pop_back();
 	}
 }
 
 void solve() {
-	
+
 	dfs(0, 0);
-	
+
 }
 
 int main() {
