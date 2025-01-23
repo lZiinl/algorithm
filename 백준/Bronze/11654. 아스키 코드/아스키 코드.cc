5e55457e6361492662905455fa31d9f1019c53d5
JH.CHO <149471946+lZiinl@users.noreply.github.com>
2025-01-23 15:40:09 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_MO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+char c;
+
+void input() {
+	
+}
+
+void solve() {
+	
+}
+
+int main() {
+
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	//freopen("sample.txt", "r", stdin);
+
+	//input();
+
+	cin >> c;
+	cout << int(c);
+	//solve();
+
+	return 0;
+}
