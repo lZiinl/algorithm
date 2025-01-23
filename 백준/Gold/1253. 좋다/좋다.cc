b6746c19dd098472fd142b315ced6047daa24650
JH.CHO <149471946+lZiinl@users.noreply.github.com>
2025-01-23 20:58:54 +0900

    [Gold IV] Title: 좋다, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/1253. 좋다/좋다.cc
+++ 백준/Gold/1253. 좋다/좋다.cc
@@ -1,69 +1,61 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include <iostream>
-#include <algorithm>
+#include<iostream>
+#include<vector>
+#include<algorithm>
 
 using namespace std;
 
 int N;
-long long num[2001];
-int cnt;
-
-void input() {
-	cin >> N;
-
-	for (int i = 0; i < N; i++) {
-		cin >> num[i];
-	}
-	sort(num, num + N);
-}
-
-void solve() {
-	for (int i = 0; i < N; i++){
-		for (int j = 0; j < N; j++){
-			if (i == j) continue;
-			int flag = 0;
-			long long temp = num[i] - num[j];
-
-			int sp = 0;
-			int ep = N;
-
-			while (sp < ep) {
-				int mid = (sp + ep) / 2;
-
-				if (temp - num[mid] > 0) {
-					sp = mid + 1;
-				}
-				else if (temp - num[mid] < 0) {
-					ep = mid;
-				}
-				else {
-					if (j == mid || i == mid) {
-						sp = mid + 1;
-					}
-					else {
-						cnt++;
-						flag = 1;
-						break;
-					}
-				}
-			}
-
-			if (flag == 1) {
-				break;
-			}
-		}
-	}
-}
 
 int main() {
 
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
 	//freopen("sample.txt", "r", stdin);
 
-	input();
-	if (N > 2) {
-		solve();
+	cin >> N;
+
+	vector<int> v(N, 0);
+
+	for (int i = 0; i < N; i++){
+		cin >> v[i];
 	}
-	cout << cnt;
+
+	sort(v.begin(), v.end());
+
+	int result = 0;
+
+	for (int k = 0; k < N; k++){
+		long find = v[k];
+
+		int i = 0;
+		int j = N - 1;
+
+		while (i < j) {
+			if (v[i] + v[j] == find) {
+				if (i != k && j != k) {
+					result++;
+					break;
+				}
+				else if (i == k) {
+					i++;
+				}
+				else if (j == k) {
+					j--;
+				}
+			}
+			else if (v[i] + v[j] < find) {
+				i++;
+			}
+			else {
+				j--;
+			}
+		}
+	}
+
+	cout << result;
 
 	return 0;
 }
