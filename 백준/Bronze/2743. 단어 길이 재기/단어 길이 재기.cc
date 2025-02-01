c5851953f3e57d121a9efbf735299b5384109ba5
JH.CHO <149471946+lZiinl@users.noreply.github.com>
2025-02-01 22:59:51 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,15 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include<cstring>
+#include<string>
+
+using namespace std;
+
+int main() {
+
+	string s;
+	cin >> s;
+	cout << s.length();
+
+	return 0;
+}
