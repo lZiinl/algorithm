101ba67073936fcd9ce662dfb5ad189ebb63612e
JH.CHO <149471946+lZiinl@users.noreply.github.com>
2025-03-04 22:03:12 +0900

    [Bronze IV] Title: 기찍 N, Time: 8 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.cc
@@ -0,0 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    int N;
+    cin >> N;
+
+    for (int i = N; i > 0; i--){
+        cout << i << "\n";
+    }
+
+    return 0;
+}
