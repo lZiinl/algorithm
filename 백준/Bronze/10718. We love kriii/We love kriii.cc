0f84e8853579007980cd636c37fd4b48aed1228e
JH.CHO <149471946+lZiinl@users.noreply.github.com>
2025-03-26 23:05:08 +0900

    [Bronze V] Title: We love kriii, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10718. We love kriii/We love kriii.cc
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //freopen("sample.txt", "r", stdin);
+
+    cout << "강한친구 대한육군\n";
+    cout << "강한친구 대한육군\n";
+
+    return 0;
+}
