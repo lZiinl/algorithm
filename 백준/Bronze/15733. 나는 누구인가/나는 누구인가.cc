8117aa2f1140ff612cf939c02172a566227c5e99
JH.CHO <149471946+lZiinl@users.noreply.github.com>
2025-04-01 22:08:37 +0900

    [Bronze V] Title: 나는 누구인가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15733. 나는 누구인가/나는 누구인가.cc
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    //freopen("sample.txt", "r", stdin);
+
+    cout << "I'm Sexy";
+
+    return 0;
+}
