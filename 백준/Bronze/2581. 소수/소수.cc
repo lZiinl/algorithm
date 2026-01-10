93c8c6c83d0f6669d2bd1d4b70cca3365a30b2e4
JH.CHO <149471946+lZiinl@users.noreply.github.com>
2026-01-10 22:41:21 +0900

    [Bronze II] Title: 소수, Time: 12 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.cc
@@ -0,0 +1,52 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include<iostream>
+
+using namespace std;
+
+int arr[10001];
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	//freopen("sample.txt", "r", stdin);
+
+	int M, N;
+	cin >> M >> N;
+
+	for (int i = 2; i <= N; i++)
+	{
+		if (arr[i] == 1) continue;
+		else {
+			for (int j = 2; j <= N; j++)
+			{
+				if (i * j > 10000) continue;
+				arr[i*j] = 1;
+			}
+		}
+	}
+
+	int res = 0;
+	int ans = 9999999;
+	for (int i = M; i <= N; i++)
+	{
+		if (i == 1) continue;
+		if (arr[i] == 0) {
+			res += i;
+
+			if (ans > i) {
+				ans = i;
+			}
+		}
+	}
+
+	if (res == 0) {
+		cout << -1;
+		return 0;
+	}
+
+	cout << res << "\n" << ans;
+	return 0;
+}
